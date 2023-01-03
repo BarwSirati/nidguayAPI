15f6b0052d59eb47b7ab1631f828255f8596965a
BarwSirati <sirati10546@gmail.com>
2023-01-03 23:03:15 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FacultyModule } from './faculty/faculty.module';
@@ -7,6 +7,7 @@ import { DataSource } from 'typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { getEnvPath } from './common/helper/env.helper';
 import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 @Module({
@@ -18,6 +19,9 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
   controllers: [AppController],
   providers: [AppService, TypeOrmConfigService],
 })
-export class AppModule {
+export class AppModule implements NestModule {
   constructor(private datasource: DataSource) {}
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('faculty');
+  }
 }
