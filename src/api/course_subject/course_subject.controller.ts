1d8d0e054a2d6fcd2b49af59500833f1e07e9a70
BarwSirati <sirati10546@gmail.com>
2023-01-07 23:50:21 +0700

    security 1.0

--- src/api/course_subject/course_subject.controller.ts
+++ src/api/course_subject/course_subject.controller.ts
@@ -6,18 +6,25 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Roles } from '../auth/decorator/roles.decorator';
+import { JwtGuard } from '../auth/guard/jwt.guard';
+import { RolesGuard } from '../auth/guard/roles.guard';
 import { CourseSubjectService } from './course_subject.service';
 import { CreateCourseSubjectDto } from './dto/create-course_subject.dto';
 import { UpdateCourseSubjectDto } from './dto/update-course_subject.dto';
 import { CourseSubject } from './entities/course_subject.entity';
+import { Role } from '../../shared/interfaces/role.interface';
 
 @Controller('courseSubject')
 @ApiTags('courseSubject')
 export class CourseSubjectController {
   constructor(private readonly courseSubjectService: CourseSubjectService) {}
 
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Post()
   async create(@Body() createCourseSubjectDto: CreateCourseSubjectDto) {
     return await this.courseSubjectService.create(createCourseSubjectDto);
@@ -33,6 +40,8 @@ export class CourseSubjectController {
     return await this.courseSubjectService.findOne(id);
   }
 
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -41,6 +50,8 @@ export class CourseSubjectController {
     return this.courseSubjectService.update(id, updateCourseSubjectDto);
   }
 
+  @UseGuards(JwtGuard, RolesGuard)
+  @Roles(Role.Admin)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.courseSubjectService.remove(id);
