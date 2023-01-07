41ad057cbfb6a15b857003617432a7fbd8535160
BarwSirati <sirati10546@gmail.com>
2023-01-08 00:31:39 +0700

    UPDATE DOCUMENT

--- src/api/auth/dto/login.dto.ts
+++ src/api/auth/dto/login.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty()
   @IsString()
   @MinLength(8)
   @MaxLength(8)
   username: string;
 
+  @ApiProperty()
   @IsString()
   password: string;
 }
--- src/api/course_subject/dto/create-course_subject.dto.ts
+++ src/api/course_subject/dto/create-course_subject.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCourseSubjectDto {
+  @ApiProperty()
   @IsString()
   @MinLength(8)
   @MaxLength(8)
   id: string;
 
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsNumber()
   credit: number;
 }
